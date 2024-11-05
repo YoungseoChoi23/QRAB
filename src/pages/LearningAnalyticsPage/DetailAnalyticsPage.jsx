76f76786407940d1e9212b416347be2fcdde8147
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-11-05 17:47:21 +0900

    UI : 학습 분석 subnavbar 컴포넌트 연결

--- src/pages/LearningAnalyticsPage/DetailAnalyticsPage.jsx
+++ src/pages/LearningAnalyticsPage/DetailAnalyticsPage.jsx
@@ -3,7 +3,9 @@ import PageDefauleTemplate from "../../components/Common/PageDefauleTemplate";
 const DetailAnalyticsPage = () => {
   return (
     <>
-      <PageDefauleTemplate pageName="학습 분석"></PageDefauleTemplate>
+      <PageDefauleTemplate pageName="학습 분석">
+        <DetailAnalyticsPage />
+      </PageDefauleTemplate>
     </>
   );
 };
